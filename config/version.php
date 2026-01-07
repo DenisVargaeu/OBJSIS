a42f38e247b5dac4685ae9722f5a980522e4a8c8
DenisVargaeu <mail@denisvarga.eu>
2026-01-07 12:01:27 +0100

    Update OBJSIS_VERSION to 2.3.0

--- config/version.php
+++ config/version.php
@@ -3,6 +3,6 @@
 // This file is tracked by Git and used for the built-in updater.
 
 if (!defined('OBJSIS_VERSION')) {
-    define('OBJSIS_VERSION', '2.2.1');
+    define('OBJSIS_VERSION', '2.3.0');
 }
 ?>
